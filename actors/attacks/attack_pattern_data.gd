1b93dac7f883c91d705ac79ab928381f502a97ca
anynoise00 <anynoise00@gmail.com>
2021-01-01 13:15:15 -0300

    Fixed attack spawner rotation bug

--- actors/attacks/attack_pattern_data.gd
+++ actors/attacks/attack_pattern_data.gd
@@ -1,7 +1,7 @@
 extends Resource
 class_name AttackPatternData
 
-export(float, 0, 360) var rotation_speed = 0
+export(float, 0, 360) var rotation_per_attack = 0
 export(float, 0.1, 0.5) var inbetween_delay = 0.1
 
 export(Array, Resource) var attack_datas = []
--- world/attack_spawner/attack_spawner.gd
+++ world/attack_spawner/attack_spawner.gd
@@ -3,7 +3,7 @@ extends Node
 const PROJECTILE_SCENE = preload("res://projectile/Projectile.tscn")
 
 func execute_attack(pattern: AttackPatternData, direction: Vector2, position: Vector2, collision_layer: int):
-	var dir: Vector2 = direction
+	var dir: Vector2 = direction.normalized()
 	var proj_node = get_tree().get_root().get_node("Main/World/Projectiles")
 	if proj_node == null:
 		var n = Node.new()
@@ -12,16 +12,26 @@ func execute_attack(pattern: AttackPatternData, direction: Vector2, position: Ve
 		proj_node = n
 	
 	for attack_data in pattern.attack_datas:
-		var proj_dir = dir.rotated(-attack_data.spread / 2)
+		var rotation_step = 0
+		var proj_initial_rot = 0
 		
-		for p_data in attack_data.projectiles:
+		if attack_data.spread == 360:
+			rotation_step = attack_data.spread / attack_data.projectiles.size()
+			proj_initial_rot = dir.angle()
+		else:
+			rotation_step = attack_data.spread / (attack_data.projectiles.size() - 1)
+			proj_initial_rot = dir.angle() - deg2rad(attack_data.spread / 2)
+		
+		for i in range(attack_data.projectiles.size()):
 			var proj = PROJECTILE_SCENE.instance()
+			var angle = proj_initial_rot + deg2rad(rotation_step * i)
+			var rot_vec = Vector2(cos(angle), sin(angle))
+			
 			proj_node.add_child(proj)
 			
 			proj.position = position
-			proj.setup_projectile(p_data, proj_dir, collision_layer)
-			
-			proj_dir = proj_dir.rotated(attack_data.spread / attack_data.projectiles.size() - 1)
+			proj.setup_projectile(attack_data.projectiles[i],
+				rot_vec, collision_layer)
 		
-		dir = dir.rotated(deg2rad(pattern.rotation_speed * pattern.inbetween_delay))
+		dir = dir.rotated(deg2rad(pattern.rotation_per_attack))
 		yield(get_tree().create_timer(pattern.inbetween_delay), "timeout")
