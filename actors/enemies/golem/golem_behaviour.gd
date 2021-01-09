409b9c1466f4c10902e2d76f48d9a3d66e7c9422
anynoise00 <anynoise00@gmail.com>
2021-01-09 19:05:03 -0300

    Golem Behaviour added

---
+++ actors/enemies/golem/golem_behaviour.gd
@@ -0,0 +1,56 @@
+extends Enemy
+
+enum State {
+	IDLE,
+	MOVE,
+	ATTACK,
+}
+
+var current_state = State.IDLE
+
+func _ready():
+	._ready()
+	attack_cooldown_timer.wait_time += (randf() - 0.2) * 5
+	player_min_distance_to_attack = pow((12 + randi() % 4 + 1) * 16, 2)
+
+func _physics_process(delta):
+	_flip_to_player()
+	
+	match(current_state):
+		State.IDLE:
+			if anim_player.current_animation != Globals.ANIM_IDLE:
+				anim_player.play(Globals.ANIM_IDLE)
+			
+			if (attack_cooldown_timer.is_stopped()
+					&& player_detector.get_player_sqr_distance_to(global_position)
+					< player_min_distance_to_attack):
+				current_state = State.ATTACK
+				
+			elif(player_detector.get_player_sqr_distance_to(global_position) >
+					min_distance_to_player):
+				current_state = State.MOVE
+		
+		State.MOVE:
+			if anim_player.current_animation != Globals.ANIM_MOVE:
+				anim_player.play(Globals.ANIM_MOVE)
+			
+			if (attack_cooldown_timer.is_stopped()
+					&& player_detector.get_player_sqr_distance_to(global_position)
+					< player_min_distance_to_attack):
+				current_state = State.ATTACK
+				
+			elif(player_detector.get_player_sqr_distance_to(global_position) <=
+					min_distance_to_player):
+				current_state = State.IDLE
+			
+			move(-player_detector.get_player_direction_to(global_position))
+		
+		State.ATTACK:
+			if anim_player.current_animation != Globals.ANIM_ATTACK:
+				anim_player.play(Globals.ANIM_ATTACK)
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	match anim_name:
+		Globals.ANIM_ATTACK:
+			current_state = State.IDLE
+			attack_cooldown_timer.start()
