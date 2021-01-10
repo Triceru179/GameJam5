07da2e31423da13231784b24344b0f271643dc7d
anynoise00 <anynoise00@gmail.com>
2021-01-10 00:28:55 -0300

    I think I fixed the crash

--- actors/enemies/player_detector.gd
+++ actors/enemies/player_detector.gd
@@ -6,6 +6,7 @@ func _ready():
 	yield(get_tree(), "idle_frame")
 	
 	player = get_tree().get_current_scene().get_node("World/Player")
+	player.connect("tree_exiting", self, "_on_Player_tree_exiting")
 
 func get_player_direction_to(point: Vector2):
 	if player != null:
@@ -18,3 +19,6 @@ func get_player_sqr_distance_to(point: Vector2):
 		return (point - player.global_position).length_squared()
 	else:
 		return 128000
+
+func _on_Player_tree_exiting():
+	player = null
