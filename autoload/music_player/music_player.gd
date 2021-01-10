251ba20f732662c658b63efb032d06ab854f025b
anynoise00 <anynoise00@gmail.com>
2021-01-09 21:57:17 -0300

    Music player fixed

--- autoload/music_player/music_player.gd
+++ autoload/music_player/music_player.gd
@@ -1,24 +1,24 @@
 extends Node
 
-var playlist := []
+var pl := []
 var last_song_index := -1
 
 func change_song():
 	var song
 	if last_song_index == -1:
-		var i = randi() % playlist.size()
-		song = playlist[i]
+		var i = randi() % pl.size()
+		song = pl[i]
 		
 		last_song_index = i
 		
 	else:
-		var temp_pl = playlist
+		var temp_pl = pl
 		temp_pl.remove(last_song_index)
 		
 		var i = randi() % temp_pl.size()
 		song = temp_pl[i]
 		
-		last_song_index = playlist.find(temp_pl[i])
+		last_song_index = pl.find(temp_pl[i])
 	
 	var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
 	if song != audio_stream_player.stream:
@@ -27,13 +27,9 @@ func change_song():
 
 func change_playlist(playlist: Array):
 	last_song_index = -1
-	self.playlist = playlist
-	var song
+	pl = playlist
 	
-	if playlist.size() == 1:
-		song = playlist[0]
-		
-	elif playlist.size() <= 0:
+	if pl.size() <= 0:
 		push_error("Playlist can't contain less than one song.")
 	
 	change_song()
